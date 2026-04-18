2702714a047bdd9c991aecc2708cd388d90ec48c
vizurth <81080666+vizurth@users.noreply.github.com>
2026-04-18 22:18:41 +0300

    feat: add func in main.go for error checking

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,35 +12,32 @@ import (
 )
 
 func main() {
-	log, err := logger.New()
-	if err != nil {
+	if err := run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
 
-	defer func() {
-		if err := log.Sync(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}()
+func run() error {
+	log, err := logger.New()
+	if err != nil {
+		return fmt.Errorf("init logger: %w", err)
+	}
+	defer func() { _ = log.Sync() }()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	ctx = logger.With(ctx, log)
+	ctx := logger.With(context.Background(), log)
 
 	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal(ctx, "failed to load config", zap.Error(err))
-	}
-	application, err := app.New(ctx, *cfg)
-	if err != nil {
-		log.Fatal(ctx, "failed to create application", zap.Error(err))
-		return
+		log.Error(ctx, "failed to load config", zap.Error(err))
+		return fmt.Errorf("load config: %w", err)
 	}
 
-	if err := application.Run(ctx); err != nil {
-		log.Fatal(ctx, "application error", zap.Error(err))
-		return
+	application, err := app.New(ctx, cfg)
+	if err != nil {
+		log.Error(ctx, "failed to create application", zap.Error(err))
+		return fmt.Errorf("init app: %w", err)
 	}
+
+	return application.Run(ctx)
 }
